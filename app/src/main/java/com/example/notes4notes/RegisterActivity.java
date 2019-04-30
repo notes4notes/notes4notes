d607aa3a828b25d67e198ef5243a835d624350e3
Lovizac <lovizac@gmail.com>
2019-04-30 18:52:01 -0400

    register activity added

--- app/src/main/java/com/example/notes4notes/RegisterActivity.java
+++ app/src/main/java/com/example/notes4notes/RegisterActivity.java
@@ -1,13 +1,66 @@
 package com.example.notes4notes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.parse.SignUpCallback;
+
+
 public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = "Register Activity";
+    private EditText etName;
+    private EditText etUsername;
+    private EditText etEmail;
+    private EditText etPass;
+    private Button btnSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        etName = findViewById(R.id.etName);
+        etUsername = findViewById(R.id.etUsername);
+        etEmail = findViewById(R.id.etEmail);
+        etPass = findViewById(R.id.etPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = etName.getText().toString();
+                String username = etUsername.getText().toString();
+                String email = etEmail.getText().toString();
+                String password = etPass.getText().toString();
+                if (user.length() <= 0 || username.length() <= 0 || email.length() <= 0 || password.length() <= 0) {
+                    Toast.makeText(RegisterActivity.this, "Missing a field", Toast.LENGTH_LONG).show();
+                } else {
+                    ParseUser newUser = new ParseUser();
+                    newUser.setUsername(username);
+                    newUser.setEmail(email);
+                    newUser.setPassword(password);
+                    newUser.signUpInBackground(new SignUpCallback() {
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Toast.makeText(RegisterActivity.this, "Thanks for signing up", Toast.LENGTH_LONG).show();
+                                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(i);
+                                finish();
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "Failed to sign up", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
-} // end of class
+}
