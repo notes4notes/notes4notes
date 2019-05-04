99b26dc4593c296523a6811517bf76ebc3dab267
mtkhawaja <36654508+mtkhawaja@users.noreply.github.com>
2019-05-03 21:49:28 -0400

    -Added application logo -Developed Post Layout File. -Removed login bypass.

--- app/src/main/java/com/example/notes4notes/LoginActivity.java
+++ app/src/main/java/com/example/notes4notes/LoginActivity.java
@@ -91,8 +91,6 @@ public class LoginActivity extends AppCompatActivity {
                 String usernameTxt = username.getText().toString();
                 String passwordTxt = password.getText().toString();
                 login(usernameTxt, passwordTxt);
-                // to do, remove later
-                afterLogInActivity();
             }
         });
 
--- app/src/main/java/com/example/notes4notes/Models/Post.java
+++ app/src/main/java/com/example/notes4notes/Models/Post.java
@@ -8,7 +8,27 @@ public class Post extends ParseObject {
     private static final  String KEY_POST_TITLE        = "title"       ;
     private static final  String KEY_POST_DESCRIPTION  = "description" ;
     private static final  String KEY_POST_CREATED_AT   = "createdAt"   ;
-    private static final  String KEY_USER_EMAIL        = "userEmail"   ;
+    private static final  String KEY_POST_RATING       = "postRating"  ;
+    private static final  String KEY_POST_FILE         = "postFile"    ;
     /* Getter Methods */
 
+    public static String getKeyPostTitle() {
+        return KEY_POST_TITLE;
+    }
+
+    public static String getKeyPostDescription() {
+        return KEY_POST_DESCRIPTION;
+    }
+
+    public static String getKeyPostCreatedAt() {
+        return KEY_POST_CREATED_AT;
+    }
+
+    public static String getKeyPostRating() {
+        return KEY_POST_RATING;
+    }
+
+    public static String getKeyPostFile() {
+        return KEY_POST_FILE;
+    }
 } // end of Class
