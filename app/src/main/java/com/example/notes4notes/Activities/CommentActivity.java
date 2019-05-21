1c9333188bf3f33fdb86eb54fd81b63046bc3a7b
mtkhawaja <36654508+mtkhawaja@users.noreply.github.com>
2019-05-20 22:33:13 -0400

    -Fixes
    Signed-off-by: mtkhawaja <36654508+mtkhawaja@users.noreply.github.com>

--- app/src/main/java/com/example/notes4notes/Activities/CommentActivity.java
+++ app/src/main/java/com/example/notes4notes/Activities/CommentActivity.java
@@ -1,6 +1,27 @@
 package com.example.notes4notes.Activities;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.notes4notes.R;
+
 public class CommentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+        Button btn = findViewById(R.id.commentSubmitBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),  "Comment Added!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
 }
